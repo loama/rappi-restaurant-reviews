723588dc82ffe61b416d166a366c9d85df1ffa47
Jaime Lopez <loama18@gmail.com>
2019-06-25 22:02:39 -0500

    add step logic

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="reviews">
-    <div class="cards">
+    <div class="cards" v-bind:class="current_step_in_letters">
       <div class="card restaurant">
         <span class="title">¿Qué tal estuvo tu experiencia?</span>
 
@@ -46,8 +46,15 @@
       <img src="../assets/wlb.png">
     </div>
 
-    <div class="previous-button"></div>
-    <div class="next-button"></div>
+    <div class="previous-button"
+         v-on:click="current_step = current_step - 1"
+         v-bind:class="current_step_in_letters">
+      <img src="../assets/keyboard_backspace.svg">
+    </div>
+
+    <div class="next-button"
+         v-on:click="current_step = current_step + 1"
+         v-bind:class="[current_step_in_letters, next_active ? 'active' : '']"> {{next_text}} </div>
 
     <div class="status-all"></div>
     <div class="status-current"></div>
@@ -57,9 +64,47 @@
 
 <script>
 export default {
+  computed: {
+    current_step_in_letters () {
+      if (this.current_step === 0) {
+        return 'zero'
+      } else if (this.current_step === 1) {
+        return 'one'
+      } else {
+        return 'two'
+      }
+    },
+    dish_review_valid () {
+      if (this.dish_review_one && this.dish_review_two && this.dish_review_three) {
+        return true
+      } else {
+        return false
+      }
+    },
+    next_active () {
+      if (this.current_step === 0 && this.emoji_active !== null) {
+        return true
+      } else if (this.current_step === 1 && this.dish_review_valid) {
+        return true
+      } else {
+        return false
+      }
+    },
+    next_text () {
+      if (this.current_step < 1) {
+        return 'Siguiente'
+      } else {
+        return 'Terminar'
+      }
+    }
+  },
   data () {
     return {
-      emoji_active: null
+      emoji_active: null,
+      dish_review_one: null,
+      dish_review_two: null,
+      dish_review_three: null,
+      current_step: 0
     }
   },
   name: 'reviews'
@@ -95,7 +140,17 @@ export default {
 
     .cards
       margin-top: 40px
+      transition: all 0.3s
       width: 500px
+
+      &.zero
+        transform: translate3d(0, 0, 0)
+
+      &.one
+        transform: translate3d(-248px, 0, 0)
+
+      &.two
+        transform: translate3d(-504px, 0, 0)
 
     .card
       background: #FFF
@@ -137,14 +192,46 @@ export default {
       font-size: 15px
       margin-top: 48px
 
+    .previous-button
+      border: 1px solid #D8D8D8
+      border-radius: 8px
+      bottom: 16px
+      cursor: pointer
+      height: 46px
+      left: -36px
+      position: absolute
+      transform: translate3d(0, 0, 0)
+      transition: all 0.3s
+      width: 48px
+
+      &.one
+        transform: translate3d(48px, 0, 0)
+
+      img
+        margin: 10px 0
+        opacity: 0.5
+
     .next-button
       background: #F75D63
       border-radius: 8px
       bottom: 16px
+      color: #FFF
+      cursor: pointer
       height: 48px
       left: 12px
+      line-height: 48px
+      pointer-events: none
       position: absolute
+      transform: translate3d(0, 0, 0)
+      transition: all 0.3s
       width: 256px
+
+      &.active
+        pointer-events: all
+
+      &.one
+        transform: translate3d(56px, 0, 0)
+        width: 200px
 
     .status-all
       background: #DDD
