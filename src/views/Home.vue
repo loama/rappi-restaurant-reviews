3b45cedf0fa0fa4853a1a8776e5bd72d0cec2c40
Jaime Lopez <loama18@gmail.com>
2019-06-25 19:55:48 -0500

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="../assets/iphonex.png" class="iphonex">
+    <img src="../assets/rappi_background.png" class="rappi_background">
+    <Reviews/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Reviews from '@/components/Reviews.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Reviews
   }
 }
 </script>
+
+<style lang="sass">
+  .iphonex
+    position: absolute
+    top: 8px
+    left: calc(50vw - 160px)
+    width: 320px
+    z-index: 20
+
+  .rappi_background
+    position: absolute
+    top: 24px
+    left: calc(50vw - 140px)
+    width: 280px
+    z-index: 1
+</style>
